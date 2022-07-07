a74ae8d3217b3047806487d791cbc8500eeed8b5
Louis <louisgrunberg@MBP-von-Louis.home>
2022-07-07 16:41:53 +0200

    test

---
+++ build/CMakeFiles/math_parser.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/math_parser.dir/src/main.cpp.o"
+  "CMakeFiles/math_parser.dir/src/main.cpp.o.d"
+  "math_parser"
+  "math_parser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/math_parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
